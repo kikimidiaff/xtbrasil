accdbe74b0cb420faff7baa32e7cadacfe3e8d16
kikimidiaff <midiaffon@gmail.com>
2026-03-26 16:42:18 -0300

    Tabela atualizada

--- script.js
+++ script.js
@@ -19,26 +19,26 @@ buttons.forEach(button => {
 // 📊 RANKING (COM SETA MANUAL)
 // =======================
 const rankingData = [
-  { posicao: 1, variacao: "🔼", time: "EAGLES EMU", pontos: 422, kills: 277 },
-  { posicao: 2, variacao: "🔽", time: "XR7 FOXX", pontos: 318, kills: 204 },
-  { posicao: 3, variacao: "➖", time: "HKS EMU", pontos: 241, kills: 150 },
-  { posicao: 4, variacao: "🔼", time: "DRAGON AURA", pontos: 238, kills: 149 },
-  { posicao: 5, variacao: "🔼", time: "WKD EVOS", pontos: 234, kills: 134 },
-  { posicao: 6, variacao: "➖", time: "LEGENDS", pontos: 224, kills: 147 },
-  { posicao: 7, variacao: "🔼", time: "GOD GG", pontos: 210, kills: 107 },
-  { posicao: 8, variacao: "🔼", time: "LB3 E-SPORTS", pontos: 208, kills: 127 },
-  { posicao: 9, variacao: "🔽", time: "CTRL C + CTRL V", pontos: 179, kills: 97 },
-  { posicao: 10, variacao: "🔽", time: "BRIOCUDOS", pontos: 178, kills: 109 },
-  { posicao: 11, variacao: "🔼", time: "HKS ACADEMY", pontos: 171, kills: 86 },
-  { posicao: 12, variacao: "🔼", time: "ALL GAMES 7VEN", pontos: 166, kills: 92 },
-  { posicao: 13, variacao: "🔼", time: "STREET SPORTS", pontos: 164, kills: 96 },
-  { posicao: 14, variacao: "🔽", time: "SILENT", pontos: 162, kills: 85 },
-  { posicao: 15, variacao: "🔼", time: "MAGUINETA", pontos: 158, kills: 98 },
-  { posicao: 16, variacao: "🔽", time: "RUPTURA", pontos: 152, kills: 94 },
-  { posicao: 17, variacao: "🔽", time: "ANONYMOUS ELITE", pontos: 146, kills: 103 },
-  { posicao: 18, variacao: "🔼", time: "K ILL OR DIE", pontos: 144, kills: 94 },
-  { posicao: 19, variacao: "🔼", time: "SHARK E-SPORTS", pontos: 144, kills: 85 },
-  { posicao: 20, variacao: "🔽", time: "EAGLES", pontos: 118, kills: 65 }
+  { posicao: 1, variacao: "➖", time: "EAGLES EMU", pontos: 630, kills: 413 },
+  { posicao: 2, variacao: "🔼", time: "MAGUINETA", pontos: 471, kills: 293 },
+  { posicao: 3, variacao: "🔽", time: "XR7 FOXX", pontos: 457, kills: 295 },
+  { posicao: 4, variacao: "🔼", time: "LEGENDS", pontos: 410, kills: 266 },
+  { posicao: 5, variacao: "🔽", time: "DRAGON AURA", pontos: 407, kills: 272 },
+  { posicao: 6, variacao: "🔼", time: "ALLGAMES", pontos: 354, kills: 197 },
+  { posicao: 7, variacao: "🔼", time: "HKS ACADEMY", pontos: 325, kills: 176 },
+  { posicao: 8, variacao: "🔽", time: "WKD EVOS", pontos: 317, kills: 183 },
+  { posicao: 9, variacao: "🔽", time: "LB3 E-SPORTS", pontos: 312, kills: 194 },
+  { posicao: 10, variacao: "🔽", time: "CTRL C+ CTRL V", pontos: 312, kills: 170 },
+  { posicao: 11, variacao: "🔽", time: "HKS EMU", pontos: 281, kills: 173 },
+  { posicao: 12, variacao: "🔼", time: "ANONYMOUS ELITE", pontos: 264, kills: 174 },
+  { posicao: 13, variacao: "➖", time: "STREET SPORTS", pontos: 262, kills: 175 },
+  { posicao: 14, variacao: "➖", time: "SILENT", pontos: 257, kills: 125 },
+  { posicao: 15, variacao: "🔼", time: "K ILL OR DIE", pontos: 248, kills: 153 },
+  { posicao: 16, variacao: "➖", time: "RUPTURA", pontos: 240, kills: 136 },
+  { posicao: 17, variacao: "🔼", time: "EAGLES", pontos: 232, kills: 142 },
+  { posicao: 18, variacao: "🔽", time: "GOD GG", pontos: 210, kills: 107 },
+  { posicao: 19, variacao: "🔽", time: "BRIOCUDOS", pontos: 178, kills: 109 },
+  { posicao: 20, variacao: "🔼", time: "SOUL", pontos: 173, kills: 102 }
 ];
 
 
@@ -103,10 +103,18 @@ const resultadosPorDia = [
   {
     dia: "Quarta-feira",
     imagens: [
-      { src: "./assets/images/QUA/00H.png", horario: "00:00", data: "25/03/2026", tamanho: "liga" }
+      { src: "./assets/images/QUA/00H.png", horario: "00:00", data: "25/03/2026", tamanho: "liga" },
+      { src: "./assets/images/QUA/16H.jpeg", horario: "16:00", data: "25/03/2026", tamanho: "liga" },
+      { src: "./assets/images/QUA/18H.jpeg", horario: "18:00", data: "25/03/2026", tamanho: "liga" },
+      { src: "./assets/images/QUA/19H.jpeg", horario: "19:00", data: "25/03/2026", tamanho: "normal" },
+      { src: "./assets/images/QUA/20H.jpeg", horario: "20:00", data: "25/03/2026", tamanho: "liga" },
+      { src: "./assets/images/QUA/22H.jpeg", horario: "22:00", data: "25/03/2026", tamanho: "liga" },
+      { src: "./assets/images/SEG/22H-n.jpeg", horario: "Horário: 22:00", data: "Data: 23/03/2026", tamanho: "normal" }
     ]
   },
-  { dia: "Quinta-feira", imagens: [] },
+  { dia: "Quinta-feira", imagens: [
+    { src: "./assets/images/QUI/00H.jpeg", horario: "00:00", data: "26/03/2026", tamanho: "liga" }
+  ] },
   { dia: "Sexta-feira", imagens: [] }
 ];
 
